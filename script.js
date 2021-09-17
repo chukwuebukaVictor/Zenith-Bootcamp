7f06d7b6719d956761d8dd5010629c47d8b0118a
chukwuebukaVictor <chukwuebuka.victor.ozoede@gmail.com>
2021-09-17 01:58:41 +0100

    Implement changes of nav bar, speaker section and program section

--- script.js
+++ script.js
@@ -6,32 +6,32 @@ const speakersArray = [
   {
     name: 'Yochai Benckler',
     job: 'Ceo of at Zenith Bootcamp',
-    description: 'Youchai Studied IT at Havard high school as a fullstack web developer and also a business man. He has worked is so meny reputable companypies befors joining Zenith Bootcamp',
+    description: 'Youchai Studied IT at Havard high school as a fullstack web developer ',
   },
   {
     name: 'Kilnam Chon',
     job: 'Head front end web developer team at Zenith Bootcamp',
-    description: 'Kilnam studied IT at Barclays High school as front end web developer. He has worked is so meny reputable companypies befors joining Zenith Bootcamp',
+    description: 'Kilnam studied IT at Barclays High school as front end web developer.',
   },
   {
     name: 'SohYeong Noh',
     job: 'Head back end web developers team at Zenith Bootcamp',
-    description: 'SohYeong studied IT at Havard High school as a back end web developer. She has worked is so meny reputable companypies befors joining Zenith Bootcamp',
+    description: 'SohYeong studied IT at Havard High school as a back end web developer. ',
   },
   {
     name: 'Lila Tretikov',
     job: 'Head technical team at Zenith Bootcamp',
-    description: 'Lila studied IT at oakland High university as a fullstack web developer. She has worked is so meny reputable companypies befors joining Zenith Bootcamp',
+    description: 'Lila studied IT at oakland High university as a fullstack web developer.',
   },
   {
     name: 'Julie Leda',
     job: 'Technical team memeber at Zenith Bootcamp',
-    description: 'Julie studied IT at Havard High school. She is good in javascript and a backend web developer. She has worked is so meny reputable companypies befors joining Zenith Bootcamp',
+    description: 'Julie studied IT at Havard High school and a backend web developer. ',
   },
   {
     name: 'Ryan Merkley',
     job: 'Technical team memeber at Zenith Bootcamp',
-    description: 'Ryan studied IT at Havard High school as a front end developer. He has worked is so meny reputable companypies befors joining Zenith Bootcamp',
+    description: 'Ryan studied IT at Havard High school as a front end developer. ',
   },
 ];
 
